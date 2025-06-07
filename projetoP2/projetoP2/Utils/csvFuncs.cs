1bab5ef10c819c32bba868a4b59217e28ae9abb1
LeandroGiovani <leandrogiovani@outlook.com>
2025-06-07 10:11:33 -0300

    função incializar csv criada. Caso o inverso de csv existir (nao existir), cria o arquivo csv com o cabecalho pré definido pela variável cabecalho.

--- projetoP2/projetoP2/Utils/csvFuncs.cs
+++ projetoP2/projetoP2/Utils/csvFuncs.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace projetoP2.Utils
 {
-    class csvFuncs
+    public static class csvFuncs
     {
+        public static void InicializarCsv(string caminhoCsv, string cabecalho)
+        {
+            if (!File.Exists(caminhoCsv))
+            {
+                using (StreamWriter sw = File.CreateText(caminhoCsv))
+                {
+                    sw.WriteLine(string.Join(",", cabecalho));
+                }
+            }
+        }
     }
 }
