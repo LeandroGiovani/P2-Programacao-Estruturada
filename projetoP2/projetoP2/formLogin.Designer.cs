66d168be9fc104275b9ed92d4592f0822ca6747c
LeandroGiovani <leandrogiovani@outlook.com>
2025-06-07 09:53:12 -0300

    deisgn do layout formulário de login

---
+++ projetoP2/projetoP2/formLogin.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace projetoP2
+{
+    partial class formLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEntrar = new Button();
+            txtNome = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtSenha = new TextBox();
+            SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Location = new Point(106, 246);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(278, 41);
+            btnEntrar.TabIndex = 0;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(106, 126);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(278, 23);
+            txtNome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(106, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(106, 173);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Senha:";
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(106, 191);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(278, 23);
+            txtSenha.TabIndex = 3;
+            // 
+            // formLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(499, 419);
+            Controls.Add(label2);
+            Controls.Add(txtSenha);
+            Controls.Add(label1);
+            Controls.Add(txtNome);
+            Controls.Add(btnEntrar);
+            Name = "formLogin";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnEntrar;
+        private TextBox txtNome;
+        private Label label1;
+        private Label label2;
+        private TextBox txtSenha;
+    }
+}
